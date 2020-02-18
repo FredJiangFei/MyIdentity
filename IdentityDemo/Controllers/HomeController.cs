74491859412474d6382f3024247f279ff03e0cb3
Fred Jiang <jiangf@shinetechchina.com>
2020-02-19 06:51:04 +0800

    Init

---
+++ IdentityDemo/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
